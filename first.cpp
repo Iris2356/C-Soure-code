f81bcd56a0a38c61ff7ec045fdf5499d37cc687a
Iris2356 <lilsix.i789@gmail.com>
2025-07-01 11:57:58 +0700

    first.cpp

---
+++ first.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+ string s = "               ";
+cout << endl << endl << "\n\n\n\n";
+
+/* to show mini hear
+    using both of space and * character */
+cout << s << "   **      **     " << endl;
+cout << s << " *    * *    *    " << endl ;
+cout << s << " *           *      " << endl ;
+cout << s << "   *       *        " << endl ;
+cout << s << "      * *           " ;
+  
+  return 0;
+}
